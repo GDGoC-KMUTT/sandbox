d00df9958fa5348789e7c753cc5f3a3f6364ad1f
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-12-13 18:44:47 +0700

    add tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{jsx,js,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
