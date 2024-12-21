de7e5ea13cfb0696234667373c4a0173c54d892b
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-12-21 20:06:18 +0700

    add ProjectCardLoading

--- frontend/src/pages/private/project/index.tsx
+++ frontend/src/pages/private/project/index.tsx
@@ -4,6 +4,7 @@ import useFetchProjects from "../../../hooks/useFetchProjects.ts"
 import MainLayout from "../../../layouts/MainLayout"
 import CreateProjectModal from "../../../components/modal/CreateProjectModal.tsx"
 import useCreateProject from "../../../hooks/useCreateProject.ts"
+import ProjectCardLoading from "../../../components/loader/ProjectCardLoading.tsx"
 
 const Index = () => {
     const { data: projects, isLoading, refetch } = useFetchProjects()
@@ -25,10 +26,6 @@ const Index = () => {
         )
     }
 
-    if (isLoading) {
-        return <div>isLoading</div>
-    }
-
     const headerContent = (
         <>
             <h2>Project</h2>
@@ -37,7 +34,14 @@ const Index = () => {
         </>
     )
 
-    const bodyContent = (
+    const bodyContent = isLoading ? (
+        <div className="flex flex-wrap">
+            <ProjectCardLoading />
+            <ProjectCardLoading />
+            <ProjectCardLoading />
+            <ProjectCardLoading />
+        </div>
+    ) : (
         <div className="flex flex-wrap">
             {projects?.data.map((project, index) => (
                 <ProjectCard key={index} {...project} />
