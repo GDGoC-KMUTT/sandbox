69f740aa95275b5521019d98717a35f166f245b3
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-12-30 23:53:41 +0700

    logout modal

---
+++ frontend/src/components/modal/LogoutModal.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+interface ILogoutModal {
+    onClose: () => void
+    onLogout: () => void
+}
+
+const LogoutModal: React.FC<ILogoutModal> = ({ onClose, onLogout }) => {
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={onClose}>
+            <div className="bg-background rounded-md shadow-lg p-6 w-96" onClick={(e) => e.stopPropagation()}>
+                <h2 className="text-xl font-semibold mb-4"> Logout</h2>
+
+                <p className="text-lg mb-3">Are you sure you want to logout?</p>
+                <div className="flex justify-end space-x-4">
+                    <button
+                        className="px-6 py-2 bg-background border-2 border-form hover:bg-form hover:border-form text-foreground"
+                        onClick={onClose}
+                    >
+                        Cancel
+                    </button>
+                    <button className="px-6 text-white bg-red-400 hover:bg-red-600" onClick={onLogout}>Logout</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LogoutModal
+
--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -2,6 +2,8 @@ import { ArrowLeftIcon } from "@heroicons/react/24/solid"
 import logo2 from "../../assets/logo2.png"
 import { useAuth } from "../../stores/authHelpers"
 import { useNavigate } from "react-router-dom"
+import LogoutModal from "../modal/LogoutModal"
+import { useState } from "react"
 
 const Navbar = () => {
     const { logout } = useAuth()
@@ -9,17 +11,21 @@ const Navbar = () => {
     const goBack = () => {
         navigate(-1) // Go back to the previous page
     }
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <nav className="flex items-center justify-between w-[100vw] h-[60px] bg-background p-3 shadow-nav">
             <div className="flex items-center">
-                <div className="w-[25px] cursor-pointer bg-transparent rounded-full mr-4" onClick={goBack}>
-                    <ArrowLeftIcon className="text-foreground" />
-                </div>
+                {location.pathname !== "/project" && (
+                    <div className="w-[25px] cursor-pointer bg-transparent rounded-full mr-4" onClick={goBack}>
+                        <ArrowLeftIcon className="text-foreground" />
+                    </div>
+                )}
                 <div onClick={() => navigate("/project")} className="cursor-pointer">
                     <img src={logo2} className="w-[40px] h-[40px]" />
                 </div>
             </div>
-            <button onClick={logout}>Logout</button>
+            <button onClick={() => setIsOpen(true)}>Logout</button>
+            {isOpen && <LogoutModal onClose={() => setIsOpen(false)} onLogout={logout} />}
         </nav>
     )
 }
