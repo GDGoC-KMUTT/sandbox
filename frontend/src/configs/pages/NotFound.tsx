71076a3d951967398f60c4d93d4a1e82415c64c1
Chanakarn Limprasertsiri <minklim47@gmail.com>
2025-01-04 01:41:27 +0700

    edit not found page and remove unused pages

--- frontend/src/configs/pages/NotFound.tsx
+++ frontend/src/configs/pages/NotFound.tsx
@@ -1,5 +1,12 @@
 const NotFound = () => {
-    return <div>Not Found</div>
+    return (
+        <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+            <h1>Page Not Found</h1>
+            <a href="/project" className="text-lg mt-5 text-primary hover:text-secondary underline-offset-2 underline">
+                Go to Home Page
+            </a>
+        </div>
+    )
 }
 
 export default NotFound
